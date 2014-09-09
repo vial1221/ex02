b3e18575c213b4add42cfeb49a6b2f363780428f
OlivierTremblay9 <oli_0007@hotmail.com>
2014-09-09 09:24:02 -0400

    Change language comments

--- ex02/web/ex02.dart
+++ ex02/web/ex02.dart
@@ -14,19 +14,19 @@ num speed;
 num speed_kmh;
 num distance = 100;
 num time = 9.58;
-//punto 1
+//point 1
 x=5;
 y=7; 
 z=8;
 result= x-y/z;
 print('Point 1) x-y/z = ${result}');
-//punto 2
+//point 2
 cercle_area=PI*pow(10 , 2);
 print('Point 2) Circle Area = ${cercle_area}');
-//punto3
+//point3
 num_of_acres=220*260/number_of_square_feet_by_acre;
 print('Point 3) Lot size = ${num_of_acres} acres');
-//punto4
+//point4
 speed=distance/time;
 speed_kmh=(distance/time)*3600/1000;
 print('Point 4) Usain Bolt speed = ${speed} m/s or ${speed_kmh} km/h');
